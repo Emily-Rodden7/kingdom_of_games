c85762bb827f1a059b562504affe76a954e1254d
Emily Rodden <emilyrodden.cg@gmail.com>
2025-09-16 20:52:18 +0100

    customized admin

--- products/models.py
+++ products/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 from django.db import models
 
-
 class Category(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
 
     def __str__(self):
         return self.name
