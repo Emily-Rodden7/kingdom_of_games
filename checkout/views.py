ca42a96c86a3be82d7c7c337c6e8a822d09c28e1
Emily Rodden <emilyrodden.cg@gmail.com>
2025-09-21 13:19:38 +0100

    Added stripe elements.

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51S9lZbBEfDEXQfbTpzIdCFXTvALMmH8kLwfPk3Jqr5bwf0WTmi8smzgmvY7wGJV62znNW08UXvo5pkpYy7xKmA0i008t1coYwR',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
