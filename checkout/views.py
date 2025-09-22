b4d098be608383fedee4c995b2c05ac2b882002c
Emily Rodden <emilyrodden.cg@gmail.com>
2025-09-22 08:07:40 +0100

    Updated checkout urls.py and views.py.

--- checkout/views.py
+++ checkout/views.py
@@ -27,6 +27,7 @@ def cache_checkout_data(request):
             processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
 
+
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
@@ -80,13 +81,12 @@ def checkout(request):
         else:
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
-
     else:
         bag = request.session.get('bag', {})
         if not bag:
             messages.error(request, "There's nothing in your bag at the moment")
             return redirect(reverse('products'))
-        
+
         current_bag = bag_contents(request)
         total = current_bag['grand_total']
         stripe_total = round(total * 100)
@@ -95,7 +95,7 @@ def checkout(request):
             amount=stripe_total,
             currency=settings.STRIPE_CURRENCY,
         )
-        
+
         order_form = OrderForm()
 
         if not stripe_public_key:
@@ -110,7 +110,6 @@ def checkout(request):
         }
 
         return render(request, template, context)
-        # end of the corrected indentation
 
 
 def checkout_success(request, order_number):
@@ -122,7 +121,7 @@ def checkout_success(request, order_number):
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
-    
+
     if 'bag' in request.session:
         del request.session['bag']
 
@@ -130,4 +129,5 @@ def checkout_success(request, order_number):
     context = {
         'order': order,
     }
+
     return render(request, template, context)
